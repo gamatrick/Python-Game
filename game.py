152fc73275ca44c95b8a9b20c562f3b88004a48d
gamatrick <val@laflyfamily.com>
2025-12-04 11:50:24 +0100

    refacto code

--- game.py
+++ game.py
@@ -11,28 +11,7 @@ sauvegarde = db['sauvegarde']
 monstre.delete_many({})
 hero.delete_many({})  
 
-# Fonction pour ajouter un monstre
-def ajouter_monstre(nom, atk, defense, pv):
-    nouveau_monstre = {'name': nom, 'ATK': atk, 'DEF': defense, 'PV': pv}
-    result = monstre.insert_one(nouveau_monstre)
-    print(f"Monstre '{nom}' ajouté (ID: {result.inserted_id})")
-    return result.inserted_id
-
-# Fonction pour ajouter un héros
-def ajouter_hero(nom, atk, defense, pv):
-    nouveau_hero = {'name': nom, 'ATK': atk, 'DEF': defense, 'PV': pv}
-    result = hero.insert_one(nouveau_hero)
-    print(f"Héros '{nom}' ajouté (ID: {result.inserted_id})")
-    return result.inserted_id
-
-def menu():
-    print("==========Menu:===========")
-    print("1. choisir un héros")
-    print("2. Ajouter un héros")
-    print("3. Ajouter un monstre")
-    print("4. Sauvegarder")
-    print("5. Quitter")
-
+choix = '0'
 
 # créations de la BDD monstre si elle n'existe pas
 if monstre.count_documents({}) == 0:
@@ -59,45 +38,102 @@ if hero.count_documents({}) == 0:
     print("Hero ajouté")
 else:
     print("Hero deja ajouté: ", hero.count_documents({}))
+
+# Fonction pour ajouter un monstre
+def ajouter_monstre(nom, atk, defense, pv):
+    nouveau_monstre = {'name': nom, 'ATK': atk, 'DEF': defense, 'PV': pv}
+    result = monstre.insert_one(nouveau_monstre)
+    print(f"Monstre '{nom}' ajouté (ID: {result.inserted_id})")
+    return result.inserted_id
+
+# Fonction pour ajouter un héros
+def ajouter_hero(nom, atk, defense, pv):
+    nouveau_hero = {'name': nom, 'ATK': atk, 'DEF': defense, 'PV': pv}
+    result = hero.insert_one(nouveau_hero)
+    print(f"Héros '{nom}' ajouté (ID: {result.inserted_id})")
+    return result.inserted_id
+
+def menu_principal():
+    print("==========Menu:===========")
+    print("1. choisir un héros")
+    print("2. Ajouter un héros")
+    print("3. Ajouter un monstre")
+    print("4. Sauvegarder")
+    print("5. Quitter")
+
+def choix_menu_principal(self):
+    choix = input("Choisissez une option: ")
+    if choix == '1':
+        print("Choisir un héros :")
+        for h in self.hero_collection.find():
+            print(f"- {h['name']} (ATK: {h['ATK']}, DEF: {h['DEF']}, PV: {h['PV']})")
+                
+        choix_hero = input("Nom de l'héros choisi: ")
+        hero = self.hero_collection.find_one({'name': choix_hero})
+        if hero:
+            print(f"Vous avez choisi l'héros {hero['name']} avec {hero['PV']} PV, {hero['ATK']} ATK et {hero['DEF']} DEF.")
+            '''
+            print (" voulez vous partir à l'aventure ?")
+            print("1. Oui")
+            print("2. Non")
+            choix = input("Choisissez une option: ")
+            if choix == '1':
+            '''
+                
+        else:
+             print("Héros introuvable.")
+
+    elif choix == '2':
+        nom = input("Nom de l'héros: ")
+        atk = int(input("ATK de l'héros: "))
+        def_ = int(input("DEF de l'héros: "))
+        pv = int(input("PV de l'héros: "))
+        ajouter_hero(nom, atk, def_, pv)
+    elif choix == '3':
+        print("Ajouter un monstre:")
+        for m in self.monstre_collection.find():
+            print(f"- {m['name']} (ATK: {m['ATK']}, DEF: {m['DEF']}, PV: {m['PV']})")
+        nom = input("Nom du monstre: ")
+        atk = int(input("ATK du monstre: "))
+        def_ = int(input("DEF du monstre: "))
+        pv = int(input("PV du monstre: "))
+        ajouter_monstre(nom, atk, def_, pv)
+    elif choix == '4':
+        print("Sauvegarde en cours...")
+        print("Sauvegarde not found!!!!!!!!!!!!!!!!")
+    
+    elif choix == '5':
+        print("Quitter le jeu.")
+        return False
+    else:
+        print("Option invalide. Veuillez choisir une option valide.")
+        
+    return True
+        
+
+
+def menu_combat(self):
+    print ("==========ennemies==========")
+    for e in self.enemies_collection.find():
+            print(f"- {e['name']} (ATK: {e['ATK']}, DEF: {e['DEF']}, PV: {e['PV']})")
+    
+    print("==========Menu:===========")
+    print("1. Attaquer")
+    print("2. Fuir")
+    print("4. Quitter")
     
    
  
 class Game:
     def __init__(self, hero_collection):
         self.hero_collection = hero_collection
+    
+    def __init__(self, monstre_collection):
+        self.monstre_collection = monstre_collection
 
     def run(self):
         while True:
-            menu()
-            choix = input("Choisissez une option: ")
-            if choix == '1':
-                print("Choisir un héros :")
-                for h in self.hero_collection.find():
-                    print(f"- {h['name']} (ATK: {h['ATK']}, DEF: {h['DEF']}, PV: {h['PV']})")
-                
-                choix_hero = input("Nom de l'héros choisi: ")
-                hero = self.hero_collection.find_one({'name': choix_hero})
-                if hero:
-                    print(f"Vous avez choisi l'héros {hero['name']} avec {hero['PV']} PV, {hero['ATK']} ATK et {hero['DEF']} DEF.")
-                else:
-                    print("Héros introuvable.")
-
-            elif choix == '2':
-                nom = input("Nom de l'héros: ")
-                atk = int(input("ATK de l'héros: "))
-                def_ = int(input("DEF de l'héros: "))
-                pv = int(input("PV de l'héros: "))
-                ajouter_hero(nom, atk, def_, pv)
-            elif choix == '3':
-                nom = input("Nom du monstre: ")
-                atk = int(input("ATK du monstre: "))
-                def_ = int(input("DEF du monstre: "))
-                pv = int(input("PV du monstre: "))
-                ajouter_monstre(nom, atk, def_, pv)
-            elif choix == '4':
-                print("Sauvegarde en cours...")
-                print("Sauvegarde not found!!!!!!!!!!!!!!!!")
-                
-            elif choix == '5':
-                print("Quitter le jeu.")
+            menu_principal()
+            continuer = choix_menu_principal(self)
+            if not continuer:
                 break
